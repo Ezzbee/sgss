497068630112af175a1ee6d664a530f30c72f214
Tunde <tundebello@hotmail.com>
2021-06-02 14:47:16 +0200

    first commit

--- backup/src/App.js
+++ backup/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProductDetails from "./components/ProductDetails";
 import Home from "./components/Home";
-
+import "./Sidebar.css";
 function App() {
   return (
     <Router>
@@ -28,8 +28,41 @@ function App() {
           </div>
         </header>
         <main>
-          <Route path="/product/:id" component={ProductDetails}></Route>
-          <Route path="/" component={Home} exact></Route>
+          <sidebar>
+            <h2>Product Categories</h2>
+            <hr />
+            <div className="sidebar__menu">
+              <div className="sidebar__link ">
+                <i className="fa fa-heart"></i>
+                <a href="#">Love</a>
+              </div>
+              <div className="sidebar__link">
+                <i className="fa fa-umbrella "></i>
+                <a href="/Profile">Protection</a>
+              </div>
+              <div className="sidebar__link">
+                <i className="fa fa-glass" aria-hidden="true"></i>
+                <a href="/appointments">Business Success</a>
+              </div>
+
+              <div className="sidebar__link">
+                <i className="fa fa-magic"></i>
+                <a href="#">Luck</a>
+              </div>
+              <div className="sidebar__link ">
+                <i className="fa fa-dollar"></i>
+                <a href="#">Financial&nbsp;problems</a>
+              </div>
+              <div className="sidebar__link">
+                <i className="fa fa-frown-o"></i>
+                <a href="#">Body&nbsp;pain&nbsp;&&nbsp;General&nbsp;illness</a>
+              </div>
+            </div>
+          </sidebar>
+          <content>
+            <Route path="/product/:id" component={ProductDetails}></Route>
+            <Route path="/" component={Home} exact></Route>
+          </content>
         </main>
         <footer>
           <p>Sey Global Spiritual Solutions - Copyright(2021)</p>
